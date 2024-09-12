02d480dbe6fce6de6b9589bed67bb0dfc1193153
LHFloveXCF <826639991@qq.com>
2024-09-12 23:56:06 +0800

    增加注释

--- day02/01-exports.js
+++ day02/01-exports.js
@@ -1,3 +1,4 @@
+// module.exports 是自定义包对外暴露的属性
 module.exports = {
     userName:'hello',
     age:18
--- day02/03-otherPackage.js
+++ day02/03-otherPackage.js
@@ -1,3 +1,4 @@
+// 引入第三方包
 const moment = require('moment')
 
 console.log(moment().format('yyyy-MM-DD hh:mm:ss'));
