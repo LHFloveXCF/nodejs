9d042bacfdab3def58a05cc7a848126c5c8390be
xuchaofeng <xuchaofeng@yoozoo.com>
2024-09-13 12:10:06 +0800

    express简单api:get/post

--- day02/06-expressWeb.js
+++ day02/06-expressWeb.js
@@ -2,6 +2,18 @@ const express = require('express')
 // 创建web服务器
 const app = express()
 
+// 查询路径获取
+app.get('/', (req, res) => {
+    console.log(req.query);
+    
+    res.send({"name": "zs", "age": 11})
+})
+
+// 动态参数获取
+app.get('/user/:id', (req, res) => {    
+    res.send(req.params)
+})
+
 app.listen(80, () => {
     console.log('express server running at http://127.0.0.1');
     
